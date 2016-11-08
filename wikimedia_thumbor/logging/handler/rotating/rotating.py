b3058eec21bea0cae2a437355b054ca8523fb0b7
Gilles Dubuc <gilles@wikimedia.org>
2016-11-08 12:01:03 +0100

    Fall back to unsafe rollover when lock cannot be acquired
    Refs T150208

--- wikimedia_thumbor/logging/handler/rotating/rotating.py
+++ wikimedia_thumbor/logging/handler/rotating/rotating.py
@@ -5,14 +5,25 @@ import os
 
 class RotatingHandler(logging.handlers.RotatingFileHandler):
     '''This class rotates log files based on size, while supporting
-    distinct python processes writing to the same file
+    distinct python processes writing to the same file.
+
+    Despite claims that lockf works on NFS shares, it didn't seem
+    to be the case when testing on Vagrant. Therefore, don't store
+    logs created with this class on a folder shared on NFS, otherwise
+    the rollover won't be multiprocess-safe.
     '''
 
     def doRollover(self):
         lockName = "%s.lock" % self.baseFilename
         f = open(lockName, 'a+')
 
-        fcntl.lockf(f.fileno(), fcntl.LOCK_EX)
+        try:
+            fcntl.lockf(f.fileno(), fcntl.LOCK_EX)
+        except IOError:
+            # Couldn't acquire a lock, give up and rollover unsafely
+            super(RotatingHandler, self).doRollover()
+            f.close()
+            return
 
         filesize = os.path.getsize(self.baseFilename)
 
