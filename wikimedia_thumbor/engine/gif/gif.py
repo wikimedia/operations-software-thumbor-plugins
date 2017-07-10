cc3617eb28cb7566eb9adc541452aa4bedf0a2e1
Gilles Dubuc <gilles@wikimedia.org>
2017-07-10 18:23:15 +0200

    Fix support for GIFs with very few colors
    Summary: Refs T169687
    
    Reviewers: fgiunchedi
    
    Reviewed By: fgiunchedi
    
    Subscribers: jenkins
    
    Maniphest Tasks: T169687
    
    Differential Revision: https://phabricator.wikimedia.org/D709

--- wikimedia_thumbor/engine/gif/gif.py
+++ wikimedia_thumbor/engine/gif/gif.py
@@ -29,6 +29,16 @@ class Engine(BaseEngine):
     def is_gif(cls, buffer):
         return buffer.startswith('GIF8')
 
+    def resize(self, width, height):
+        super(Engine, self).resize(width, height)
+        # Allow Gifsicle to add intermediate colors when resizing images.
+        # Normally, Gifsicle’s resize algorithms use input images’ color
+        # palettes without changes. When shrinking images with very few colors
+        # (e.g., pure black-and-white images), adding intermediate colors can
+        # improve the results. The following option allows Gifsicle to add
+        # intermediate colors for images that have fewer than 64 input colors.
+        self.operations.append("--resize-colors 64")
+
     def load(self, buffer, extension):
         if hasattr(self.context, 'wikimedia_original_file'):
             fname = self.context.wikimedia_original_file.name
