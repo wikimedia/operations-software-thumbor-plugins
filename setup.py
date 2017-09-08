32aecad754a062d0285b0e29f97ca6005fff089c
Gilles Dubuc <gilles@wikimedia.org>
2017-09-08 11:14:17 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 
 setup(
     name='wikimedia_thumbor',
-    version='1.2',
+    version='1.3',
     url='https://phabricator.wikimedia.org/diffusion/THMBREXT/',
     license='MIT',
     author='Gilles Dubuc, Wikimedia Foundation',
