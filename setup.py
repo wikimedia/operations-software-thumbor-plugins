5c88fb93dd1015974f19c3717edeb036f7fd26f3
gilles <gilles@wikimedia.org>
2020-05-12 16:11:58 +0200

    Version bump
    Change-Id: If4db675602d19f25f0c58a20a68bbedc00e49604

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 
 setup(
     name='wikimedia_thumbor',
-    version='2.7',
+    version='2.8',
     url='https://phabricator.wikimedia.org/diffusion/THMBREXT/',
     license='MIT',
     author='Gilles Dubuc, Wikimedia Foundation',
