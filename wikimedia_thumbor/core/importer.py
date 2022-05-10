b30f1594e05118a1d2ed77a886d270866206d08a
Roman Stolar <roman.stolar@speedandfunction.com>
2022-05-10 13:53:57 +0300

    Update copyrights.
    Replace back copyright author.
    
    Bug: T307398
    Change-Id: Ied955fa2cfd2856bcade4ab39a55a4b2e36fe1ab

--- wikimedia_thumbor/core/importer.py
+++ wikimedia_thumbor/core/importer.py
@@ -3,7 +3,7 @@
 
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
-# Copyright (c) 2022 Wikimedia Foundation
+# Copyright (c) 2017, thumbor-community
 
 from thumbor.utils import logger
 
