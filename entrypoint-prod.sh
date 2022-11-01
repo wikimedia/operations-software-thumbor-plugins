464122b9b0d8811a9fb3e39533dc4d1028f5734d
Hugh Nowlan <hnowlan@wikimedia.org>
2022-11-01 11:29:44 +0000

    Generate thumbor.key via prod entrypoint script
    Currently when thumbor is installed via apt a random key is generated
    in /etc/thumbor.key. We currently use this in production, but we lose
    this by installing thumbor via pip. Generate this key on each run of a
    thumbor container in production instead.
    
    Bug: T233196
    Change-Id: Ie51869fa6ca8be7b1bdd9a8794eddfa983a20d53

---
+++ entrypoint-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Generate a new thumbor keyfile on run to replicate the behaviour
+# previously created by the postinstall behaviour of the thumbor
+# debian package.
+
+if [ ${#} -ne 1 ]; then
+    echo "Usage: $0 port"
+    exit 1
+fi
+
+export PYTHONPATH="/srv/service:/opt/lib/python/site-packages"
+od --read-bytes 16 --output-duplicates --address-radix n --format x2 /dev/urandom | tr -d ' ' > /var/tmp/thumbor.key
+/opt/lib/python/site-packages/bin/thumbor --port ${1} --keyfile /var/tmp/thumbor.key --conf /etc/thumbor.d/
