96a710a524e58642f4759d23316f268dd1a9da8f
Hugh Nowlan <hnowlan@wikimedia.org>
2022-11-07 11:14:04 +0000

    Allow additional parameters to be passed to prod entrypoint
    For debugging it'd be nice to be able to pass other thumbor parameters
    to the thumbor command (to enable debug logging for example).
    
    Bug: T233196
    Change-Id: I210d89464c51ad76fd20e23b770f9162fa8e4419

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -3,11 +3,12 @@
 # previously created by the postinstall behaviour of the thumbor
 # debian package.
 
-if [ ${#} -ne 1 ]; then
-    echo "Usage: $0 port"
+if [ ${#} -ne 1 ] && [ ${#} -ne 2 ]; then
+    echo "Usage: $0 port [other thumbor arguments]"
+    echo "Example: $0 8888 \"-l debug\""
     exit 1
 fi
 
 export PYTHONPATH="/srv/service:/opt/lib/python/site-packages"
 od --read-bytes 16 --output-duplicates --address-radix n --format x2 /dev/urandom | tr -d ' ' > /var/tmp/thumbor.key
-/opt/lib/python/site-packages/bin/thumbor --port ${1} --keyfile /var/tmp/thumbor.key --conf /etc/thumbor.d/
+/opt/lib/python/site-packages/bin/thumbor --port ${1} --keyfile /var/tmp/thumbor.key --conf /etc/thumbor.d/ ${2}
