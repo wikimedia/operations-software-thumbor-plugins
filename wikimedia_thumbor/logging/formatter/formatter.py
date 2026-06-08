9648d15c8aaeaee07285a554d8a16c1a72d7a9b4
Hugh Nowlan <hnowlan@wikimedia.org>
2026-06-08 21:04:33 +0100

    logging: use ECS formatter
    Currently we emit a lot of unstructured logs. Emit ECS-compliant logs,
    remap some of our custom attributes to fit.
    
    In our Helm template, use of this requires us to add
    ```
    from wikimedia_thumbor.logging.formatter import ThumborFormatter
    ```
    and set
    ```
        'formatters': {
            'default': {
                '()': ThumborFormatter
            }
    ```
    in _thumbor_server.conf.
    
    Bug: T368180
    Change-Id: I0bd76336ced752b687299d90d1d47391f66fd73c

--- wikimedia_thumbor/logging/formatter/formatter.py
+++ wikimedia_thumbor/logging/formatter/formatter.py
@@ -1,13 +1,27 @@
-import logging
+import ecs_logging
 
 
-class ThumborFormatter(logging.Formatter):
-    '''This class handlers custom log formatting, to
-    allow for optional extras.
-    '''
+class ThumborFormatter(ecs_logging.StdlibFormatter):
+    """Formats log records as ECS-compliant JSON.
 
-    def format(self, record):
-        if not hasattr(record, 'url'):
-            record.url = ''
+    Remap a few of the things we add in `extras` to be a bit more
+    ECS-friendly. In the long run those should be cleaned up more
+    generally.
+    """
 
-        return super(ThumborFormatter, self).format(record)
+    # Custom record attribute -> ECS field name.
+    ECS_FIELD_MAP = {
+        "url": "url.original",
+        "thumbor-request-id": "http.request.id",
+        "port": "service.node.name",
+        "thumbor-version": "labels.thumbor_version",
+        "wikimedia-thumbor-version": "service.version",
+        "traceback": "error.stack_trace",
+    }
+
+    def format_to_ecs(self, record):
+        for src, dst in self.ECS_FIELD_MAP.items():
+            if src in record.__dict__ and record.__dict__[src]:
+                record.__dict__[dst] = record.__dict__.pop(src)
+
+        return super(ThumborFormatter, self).format_to_ecs(record)
