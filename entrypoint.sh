22cd20397dd803d9fd92b0deac709685f81bf788
Sam Wilson <sam@samwilson.id.au>
2026-09-06 09:25:42 +0800

    Set development log level to 'debug'
    Change-Id: I30763b82e7065c8d1cbee419761464fd6d7ceb9f

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 # wikimedia_thumbor is pip-installed into the image's venv, so Thumbor's
 # importlib-based extension loading finds it without any PYTHONPATH juggling.
 
-/opt/lib/venv/bin/thumbor --port 8800 --conf=thumbor.conf -a wikimedia_thumbor.app.App
+/opt/lib/venv/bin/thumbor --port 8800 --conf thumbor.conf --log-level debug --app wikimedia_thumbor.app.App
