8193072d0b1a92d96eaf980fe394cf8c518b61f6
Sam Wilson <sam@samwilson.id.au>
2026-09-09 14:29:08 +0800

    Switch from datetime.utcfromtimestamp() to .fromtimestamp()
    Avoid the deprecated method, and instead pass UTC
    to datetime.fromtimestamp().
    
    Bug: T437404
    Change-Id: I0e29f6c1397d07c45ffb007bd7356c28fcf1d731

--- wikimedia_thumbor/loader/file/__init__.py
+++ wikimedia_thumbor/loader/file/__init__.py
@@ -12,7 +12,7 @@
 # in the buffer and passes the file location via context, to mimick
 # what the other custom loaders do.
 
-from datetime import datetime
+from datetime import datetime, timezone
 from functools import partial
 from os import fstat
 from os.path import abspath, exists, join
@@ -58,7 +58,7 @@ async def load(context, path):
 
                 tornado.ioloop.IOLoop.instance().call_later(context.config.HTTP_LOADER_TEMP_FILE_TIMEOUT, partial(cleanup_temp_file, context.wikimedia_original_file.name))
 
-            result.metadata.update(size=stats.st_size, updated_at=datetime.utcfromtimestamp(stats.st_mtime))
+            result.metadata.update(size=stats.st_size, updated_at=datetime.fromtimestamp(stats.st_mtime, timezone.utc))
     else:
         result.error = 404
         result.successful = False
